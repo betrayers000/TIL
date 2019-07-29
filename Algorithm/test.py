7ea65c9c93f2c559fa6f1e0179e0a8d8f6b4e3d9
jw <betrayers000@gmail.com>
2019-07-30 08:55:08 +0900

    week3 day1

--- Algorithm/test.py
+++ Algorithm/test.py
@@ -1,18 +1,4 @@
-def checkprice(price):
-    total = 0
-    while price != []:
-        midx = price.index(max(price))
-        md = price.pop(midx)
-        for i in range(midx-1, -1, -1):
-            total = total + (md - price.pop(i))
-    return total
-
 
 T = int(input())
-for test_case in range(1, T+1):
-    days = int(input())
-    price = list(map(int, input().split()))
-    if not(2 <= days <= 1000000):
-        continue
-    total = checkprice(price)
-    print(f"#{test_case} {total}")
+for t in range(1, T+1):
+    nboard, change = input().split()
