8e27a4741b573323d7c7e6a4a7cf7b9701de1ee5
jw <betrayers000@gmail.com>
2019-08-01 20:00:33 +0900

    week3 day4

--- web/flask/app.py
+++ web/flask/app.py
@@ -1,5 +1,6 @@
 import random
 import requests
+import csv
 from bs4 import BeautifulSoup
 from flask import Flask, escape, request, render_template
 
@@ -52,6 +53,41 @@ def search():
         "#SummonerLayoutContent > div.tabItem.Content.SummonerLayoutContent.summonerLayout-summary > div.SideContent > div.TierBox.Box > div > div.TierRankInfo > div.TierRank")
     user_tier = tier.text.strip()
     return render_template('search.html', user_tier=user_tier, summoner=summoner)
+
+
+@app.route('/nono')
+def nono():
+    with open('data.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        products = list(reader)
+    return render_template('nono.html', products=products)
+
+
+@app.route('/new')
+def new():
+    return render_template('new.html')
+
+
+@app.route('/create')
+def create():
+    product = request.args.get('product')
+    category = request.args.get('category')
+    replace = request.args.get('replace')
+
+    with open('data.csv', 'a+', encoding='utf-8', newline="") as f:
+        writer = csv.writer(f)
+        product_info = [product, category, replace]
+        writer.writerow(product_info)
+    return render_template('create.html')
+
+
+@app.route('/card')
+def card():
+    with open('data.csv', 'r', encoding='utf-8') as f:
+        reader = csv.reader(f)
+        products = list(reader)
+        print(products[2])
+    return render_template('card.html', products=products)
 
 
 if __name__ == "__main__":
