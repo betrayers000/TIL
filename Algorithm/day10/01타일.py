742b552c1798fe37aab5b1bec764b9983e120190
notjw <betrayers000@gmail.com>
2019-08-23 22:52:35 +0900

    study

--- Algorithm/day10/01타일.py
+++ Algorithm/day10/01타일.py
@@ -1,21 +1,24 @@
 import sys
 sys.setrecursionlimit(1000010)
 
-def get_card(N):
-    global memo
+# def get_card(N):
+#     global memo
+#
+#     if memo[N] == 0:
+#         memo[N] = get_card(N-1) + get_card(N-2)
+#         return memo[N]
+#     else:
+#         return memo[N]
+#
 
-    if memo[N] == 0:
-        memo[N] = get_card(N-1) + get_card(N-2)
-        return memo[N]
-    else:
-        return memo[N]
-
-
-memo = [0] * 1000000
+memo = [0] * 1000010
 memo[1] = 1
 memo[2] = 2
-for i in range(1, 1000000):
-    get_card(i)
+N = 1000000
+for i in range(1, N+1):
+    if memo[i] == 0:
+        memo[i] = (memo[i-1]%15746) + (memo[i-2]%15746)
+print(memo[N])
 
 # total = 0
 # n = 99999
