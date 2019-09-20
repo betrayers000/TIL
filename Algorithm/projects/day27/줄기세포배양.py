04f18c204a29fa3738b1dc55f6e911146821d16d
jw <betrayers000@gmail.com>
2019-09-20 17:59:31 +0900

    day28

--- Algorithm/projects/day27/줄기세포배양.py
+++ Algorithm/projects/day27/줄기세포배양.py
@@ -7,49 +7,53 @@ dy = [1, 0, -1, 0]
 
 
 def breed(cell):
-    global point
-    x, y, ori, temp_ori = cell
+    global point, temp, temp_point
+    x, y, ori, n_ori = cell
     for i in range(4):
         nx = x + dx[i]
         ny = y + dy[i]
-        if point[nx][ny]:
-            if check(nx, ny, ori):
+        if point[nx][ny] == 0:
+            # if [nx, ny] not in point:
+            if [nx, ny, ori, ori] not in temp:
                 temp.append([nx, ny, ori, ori])
-                point[nx][ny] = False
-
-def check(x, y, ori):
-    global temp
-    for t in temp:
-        if t[0] == x and t[1] == y:
-            if ori > t[3]:
-                temp.remove(t)
-                return True
+                # temp_point.append([nx, ny])
             else:
-                return False
-    return True
+                for t in temp:
+                    if t[0] == nx and t[1] == ny:
+                        if t[2] >= ori:
+                            break
+                        else:
+                            temp.append([nx, ny, ori, ori])
+                            # temp_point.append([nx, ny])
+                            break
+    return
+
 
 T = int(input())
-for t in range(1, 2):
+for t in range(1, T + 1):
     N, M, K = map(int, input().split())
     board = [list(map(int, input().split())) for _ in range(N)]
     cells = []
-    point = [[True] * 3000 for _ in range(3000)]
+    point = [[0] * 3000 for _ in range(3000)]
     for i in range(N):
         for j in range(M):
             if board[i][j] != 0:
                 cells.append([i, j, board[i][j], board[i][j]])
-                point[i][j] = False
+                # point.append([i, j])
+                point[i][j] = 1
     time = 0
     while time < K:
         time += 1
         temp = []
+        temp_point = []
         for i in range(len(cells)):
             cells[i][3] -= 1
             if cells[i][3] == -1:
                 breed(cells[i])
-            if cells[i][3] + cells[i][2] == 0:
-                continue
-            temp.append(cells[i])
+            if cells[i][3] + cells[i][2] != 0:
+                temp.append(cells[i])
         cells = temp
-        print(len(cells), cells)
+        for te in temp:
+            point[te[0]][te[1]] = 1
+        # print(time, len(cells))
     print(len(cells))
