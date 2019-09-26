4607513335eec0a97a1ae3dddd9b355bdbb9b503
jw <betrayers000@gmail.com>
2019-09-26 14:45:16 +0900

    day32

--- Algorithm/projects/day31/키순서.py
+++ Algorithm/projects/day31/키순서.py
@@ -12,6 +12,7 @@ def p_sort():
             rear += 1
             q[rear] = i
             rank[i] = 1
+            count[rank[i]] += 1
     while front != rear:
         front += 1
         n = q[front]
@@ -23,6 +24,53 @@ def p_sort():
                 rear += 1
                 q[rear] = a
                 rank[a] = rank[n] + 1
+                count[rank[a]] += 1
+
+
+def check(n, z):
+    global visited, N
+    s = []
+    s.append(n)
+    cnt = 0
+    while s:
+        ns = s.pop()
+        visited[ns] = 1
+        if ns != n:
+            cnt += 1
+        for i in range(len(table[ns])):
+            a = table[ns][i]
+            if visited[a] == 0:
+                s.append(a)
+    # if z == N - cnt - 1:
+    #     for i in range(z, len(res)):
+    #         if visited[res[i]] == 0:
+    #             return False
+    #     return True
+    # return False
+
+
+def check_re(n, z):
+    global temp, visited
+    s = []
+    s.append(n)
+    cnt = 0
+    while s:
+        ns = s.pop()
+        visited[ns] = 1
+        if ns != n:
+            cnt += 1
+        # temp.append(ns)
+        for i in range(len(table_[ns])):
+            a = table_[ns][i]
+            # if a not in temp:
+            if visited[a] == 0:
+                s.append(a)
+    # if z == cnt:
+    #     for i in range(z):
+    #         if visited[res[i]] == 0:
+    #             return False
+    #     return True
+    # return False
 
 
 T = int(input())
@@ -30,17 +78,32 @@ for t in range(1, T + 1):
     N = int(input())
     M = int(input())
     table = [[]]
+    table_ = [[]]
     ind = [0] * (N + 1)
-    rank = [0] * (N + 1)
+    rank = [0] * (N +1)
+    count = [0] * (N + 1)
     res = []
     for _ in range(N):
         table += [[]].copy()
+        table_ += [[]].copy()
     for _ in range(M):
         s, e = map(int, input().split())
         table[s].append(e)
+        table_[e].append(s)
         ind[e] += 1
-    print(ind)
     p_sort()
-    print(table)
-    print(rank)
+    cnt = 0
+    print(count)
     print(res)
+    print(rank)
+    for i in range(len(res)):
+        # print(i, rank.index(i))
+        visited = [0] * (N + 1)
+        check(res[i], i)
+        check_re(res[i], i)
+        # if check(res[i], i) and check_re(res[i], i):
+        #     pass
+        # print(temp, visited)
+        if sum(visited) == N:
+            cnt += 1
+    print("#{} {}".format(t, cnt))
