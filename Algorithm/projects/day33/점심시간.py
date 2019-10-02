33c5aeafa05faf38f5d58663a594509373dca929
jw <betrayers000@gmail.com>
2019-10-02 17:09:50 +0900

    add

--- Algorithm/projects/day33/점심시간.py
+++ Algorithm/projects/day33/점심시간.py
@@ -1,7 +1,7 @@
 import sys
 
-
 sys.stdin = open('lunch.txt', 'r')
+
 
 def f(n, k):
     global minV
@@ -18,9 +18,10 @@ def f(n, k):
         return
     else:
         p[n] = 1
-        f(n+1, k)
+        f(n + 1, k)
         p[n] = 0
         f(n + 1, k)
+
 
 def moving(left, right):
     lx, ly = s[0]
@@ -31,13 +32,13 @@ def moving(left, right):
     right_table = []
     for i in left:
         x, y = h[i]
-        dis = abs(lx-x) + abs(ly-y)
+        dis = abs(lx - x) + abs(ly - y)
         left_table.append([x, y, dis])
     for i in right:
         x, y = h[i]
-        dis = abs(rx-x) + abs(ry-y)
+        dis = abs(rx - x) + abs(ry - y)
         right_table.append([x, y, dis])
-    left_table.sort(key=lambda x:x[2])
+    left_table.sort(key=lambda x: x[2])
     right_table.sort(key=lambda x: x[2])
     sec = 0
     visited_left = [0] * len(left)
@@ -45,23 +46,13 @@ def moving(left, right):
     escape_left = [0] * len(left)
     escape_right = [0] * len(right)
     # print(right_table)
+    left_temp = []
+    right_temp = []
+    # print(left, right)
     while 1:
-        for i in range(len(right_table)):
-            x, y, w = right_table[i]
-            if w == sec and visited_right[i] == 0:
-                a = check(visited_right, escape_right)
-                if a > rh:
-                    visited_right[i] = sec + rh + rh-a
-                else :
-                    visited_right[i] = sec+rh+1
-        for i in range(len(left_table)):
-            x, y, w = left_table[i]
-            if w == sec and visited_left[i] == 0:
-                a = check(visited_left, escape_left)
-                if a > lh:
-                    visited_left[i] = sec + lh + lh-a
-                else:
-                    visited_left[i] = sec+lh+1
+        # print(left_temp, right_temp)
+        # print(visited_left, visited_right)
+        # print(escape_left, escape_right)
         for i in range(len(visited_left)):
             if sec == visited_left[i] and visited_left[i] != 0:
                 escape_left[i] = 1
@@ -70,22 +61,46 @@ def moving(left, right):
                 escape_right[i] = 1
         if sum(escape_left) == len(left) and sum(escape_right) == len(right):
             break
+        for i in range(len(right_table)):
+            x, y, w = right_table[i]
+            a = check(visited_right, escape_right)
+            if visited_right[i] == 0:
+                if w == sec and a < 3:
+                    visited_right[i] = sec + rh + 1
+                elif w == sec and a > 2:
+                    right_temp.append(a)
+                else:
+                    if right_temp != [] and a < 3:
+                        right_temp.pop()
+                        visited_right[i] = sec + rh
+        for i in range(len(left_table)):
+            x, y, w = left_table[i]
+            a = check(visited_left, escape_left)
+            if visited_left[i] == 0:
+                if w == sec and a < 3:
+                    visited_left[i] = sec + lh + 1
+                elif w == sec and a > 2:
+                    left_temp.append(a)
+                else:
+                    if left_temp != [] and a < 3:
+                        left_temp.pop()
+                        visited_left[i] = sec + lh
         sec += 1
-    print(left, right, left_table, right_table)
-    print(sec, visited_left, visited_right)
+    # print(left, right, left_table, right_table)
+    # print(sec, visited_left, visited_right, lh, rh)
     return sec
+
 
 def check(v, e):
     cnt = 0
     for i in range(len(v)):
         if v[i] != 0 and e[i] == 0:
-           cnt += 1
+            cnt += 1
     return cnt
 
 
-
 T = int(input())
-for t in range(1, 2):
+for t in range(1, T+1):
     N = int(input())
     board = [list(map(int, input().split())) for _ in range(N)]
     h = []
@@ -100,4 +115,4 @@ for t in range(1, 2):
     nh = len(h)
     p = [0] * nh
     f(0, nh)
-    print(minV)
+    print("#{} {}".format(t, minV))
